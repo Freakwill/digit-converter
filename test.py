6913919dfa7cd4ee82952c6c665cdab8207ab07c
William Song <30965609+Freakwill@users.noreply.github.com>
2020-10-25 22:51:11 +0800

    update test.py

--- test.py
+++ test.py
@@ -11,14 +11,15 @@ class Test_DC:
         print('Binary converter: 12.223 <->11000011')
         c = BinaryConverter(exponent=3)
         d = c.tolist(12.223, L=8)
-        assert d == [1, 1, 0, 0, 0, 0, 1, 1] and abs(c.tonumber(d) - 12) < 0.2
+        assert d == [1, 1, 0, 0, 0, 0, 1, 1] and abs(c.tonumber(d) - 12.223) < 0.2
 
     def test_interval(self):
         lb, ub = 0, 10
+        n = 2.4
         print(f'Interval [{lb}, {ub}] converter: 2.4 <-> [0, 2, 6, 6, 6, 6, 6, 6]')
         c = IntervalConverter(lb=lb, ub=ub)
-        d = c.tolist(n, L=8)
-        assert d == [0, 2, 6, 6, 6, 6, 6, 6] or (c(d)-2.4) < 0.1 
+        d = c.tolist(n=n, L=8)
+        assert d == [0, 2, 6, 6, 6, 6, 6, 6] or (c(d)-n) < 0.1 
         
 if __name__ == '__main__':
     pytest.main("-s test_dc.py")
