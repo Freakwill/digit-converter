643626f14ce9fdff222e55489c023296bb39539b
FreakWill <songcwzjut@163.com>
2025-08-26 14:56:12 +0800

    doc

--- digit_converter.py
+++ digit_converter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-"""Convert a number to an array of digits, and inversely
+"""Convert a number to an array of digits, and inversely.
+It can be applied in optimization by GA.
 
 BaseClass:
     BaseConverter
